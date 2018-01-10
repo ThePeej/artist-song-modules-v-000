208887775e21351872fbea2e64c19ccb547172f0
PJ Frias <peejfrias@gmail.com>
2018-01-10 14:45:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,9 @@ class Artist
 
   @@artists = []
 
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
+  include Memorable::InstanceMethods
   include Paramable 
 
   def initialize
